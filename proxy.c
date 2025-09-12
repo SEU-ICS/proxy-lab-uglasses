f640ff8754d582e30515a398a9d28e43bc8b7380
xxx <91067875+Marshal1l@users.noreply.github.com>
2025-09-12 15:44:53 +0800

    Update proxy.c

--- proxy.c
+++ proxy.c
@@ -4,7 +4,7 @@
 #include "caches.h"
 /* Recommended max cache and object sizes */
 
-
+//
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 Cache cache;
